4de9e83e0cce2256142b24ef3ef3958bbf0c7244
Michael Milette <michael.milette@tngconsulting.ca>
2019-04-15 17:17:52 -0400

    D&I: Added Custom Certificate, Completion Progress, Configuratble Reports, Multi-Language Content (v2), Kopere Dashboard.

--- php-post-assemble/01-download-and-install-plugins.sh
+++ php-post-assemble/01-download-and-install-plugins.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+# Activities: Custom certificate
+wget https://moodle.org/plugins/download.php/18188/https://moodle.org/plugins/download.php/18626/mod_customcert_moodle36_2018120301.zip
+unzip https://moodle.org/plugins/download.php/18626/mod_customcert_moodle36_2018120301.zip $APP_DATA/mod
+mv $APP_DATA/mod/mod_customcert_moodle36_2018120301 $APP_DATA/mod/customcert
+rm https://moodle.org/plugins/download.php/18626/mod_customcert_moodle36_2018120301.zip
+
+# Blocks: Completion Progress
+wget https://moodle.org/plugins/download.php/18188/block_completion_progress_moodle36_2018111000.zip
+unzip block_completion_progress_moodle36_2018111000.zip $APP_DATA/blocks
+mv $APP_DATA/blocks/block_completion_progress_moodle36_2018111000 $APP_DATA/blocks/completion_progress
+rm block_completion_progress_moodle36_2018111000.zip
+
+# Blocks: Configurable Reports
+wget https://moodle.org/plugins/download.php/18988/block_configurable_reports_moodle36_2019021500.zip
+unzip https://moodle.org/plugins/download.php/18988/block_configurable_reports_moodle36_2019021500.zip $APP_DATA/blocks
+mv $APP_DATA/blocks/https://moodle.org/plugins/download.php/18988/block_configurable_reports_moodle36_2019021500 $APP_DATA/blocks/configurable_reports
+rm https://moodle.org/plugins/download.php/18988/block_configurable_reports_moodle36_2019021500.zip
+
+# Filters: Multi-Language Content (v2)
+wget https://moodle.org/plugins/download.php/17314/filter_multilang2_moodle36_2018070401.zip
+unzip filter_multilang2_moodle36_2018070401.zip $APP_DATA/filter
+mv $APP_DATA/filter/filter_multilang2_moodle36_2018070401 $APP_DATA/filter/multilang2
+rm filter_multilang2_moodle36_2018070401.zip
+
+# Local: Kopere Dashboard
 wget https://moodle.org/plugins/download.php/19201/local_kopere_dashboard_moodle36_2019031900.zip
 unzip local_kopere_dashboard_moodle36_2019031900.zip -d $APP_DATA/local
 mv $APP_DATA/local/EduardoKrausME-moodle-local-kopere_dashboard-7e28608 $APP_DATA/local/kopere_dashboard
